7667aa2e3b994625e34d5600d0be961d52fbebec
falarcon <franco.alarcon@telectronic.com>
2024-07-14 00:42:17 -0400

    Second commit

--- main.js
+++ main.js
@@ -12,19 +12,18 @@
 
 
 function calculadoraEdad(fechaNacimiento, fechaActual) {
-    const [anoNacimiento, mesNacimiento, diaNacimiento] = fechaNacimiento;
-    const [anoActual, mesActual, diaActual] = fechaActual;
+    const [anoNacimiento, mesNacimiento, diaNacimiento] = fechaNacimiento
+    const [anoActual, mesActual, diaActual] = fechaActual
 
 
-    let edadAnos = anoActual - anoNacimiento;
-    let edadMeses = mesActual - mesNacimiento;
-    let edadDias = diaActual - diaNacimiento;
+    let edadAnos = anoActual - anoNacimiento
+    let edadMeses = mesActual - mesNacimiento
+    let edadDias = diaActual - diaNacimientocd 
 
     if (edadDias < 0) {
         edadDias += 30
         edadMeses--
     }
-
 
     if (edadMeses < 0) {
         edadMeses += 12
@@ -38,14 +37,16 @@ const fechaNacimiento = [
     parseInt(prompt("Ingresa tu año de nacimiento (EJ: 1994):")),
     parseInt(prompt("Ingresa tu mes de nacimiento (Ej: 1):")),
     parseInt(prompt("Ingresa tu día de nacimiento (Ej: 17):"))
-];
+]
 
 const fechaActual = [
     parseInt(prompt("Ingresa el año actual (Ej: 2024):")),
     parseInt(prompt("Ingresa el mes actual (EJ: 7):")),
     parseInt(prompt("Ingresa el día actual (EJ: 10):"))
-];
+]
 
 const edad = calculadoraEdad(fechaNacimiento, fechaActual)
+
+console.log(`Resultado final: Años: ${edad.años}, Meses: ${edad.meses}, Días: ${edad.días}`)
 
 alert(`Usted tiene ${edad.anos} años, ${edad.meses} meses y ${edad.dias} días.`)
