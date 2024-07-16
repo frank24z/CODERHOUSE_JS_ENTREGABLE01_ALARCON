1071a36da6e55157247c5aff1c35ed366d6c2ebb
falarcon <franco.alarcon@telectronic.com>
2024-07-15 20:28:39 -0400

    sixth commit

--- main.js
+++ main.js
@@ -10,26 +10,6 @@
 //-Agrega los ciclos de iteración y/o condicionales necesarios, para que tu proyecto funcione correctamente.
 //-Integra el uso de la Consola JS y de los cuadros de diálogo Prompt, Confirm y Alert.
 
-const calculadoraEdad = (fechaNacimiento, fechaActual) => {
-    const [anoNacimiento, mesNacimiento, diaNacimiento] = fechaNacimiento
-    const [anoActual, mesActual, diaActual] = fechaActual
-
-    let edadAnos = anoActual - anoNacimiento
-    let edadMeses = mesActual - mesNacimiento
-    let edadDias = diaActual - diaNacimiento;
-    if (edadDias < 0) {
-        edadDias += 30
-        edadMeses--
-    }
-
-    if (edadMeses < 0) {
-        edadMeses += 12
-        edadAnos--;
-    }
-
-    return { anos: edadAnos, meses: edadMeses, dias: edadDias }
-}
-
 const fechaNacimiento = [
     parseInt(prompt("Ingresa tu año de nacimiento (EJ: 1994):")),
     parseInt(prompt("Ingresa tu mes de nacimiento (Ej: 1):")),
@@ -42,7 +22,28 @@ const fechaActual = [
     parseInt(prompt("Ingresa el día actual (EJ: 10):"))
 ]
 
+const calculadoraEdad = (fechaNacimiento, fechaActual) => {
+    const [anoNacimiento, mesNacimiento, diaNacimiento] = fechaNacimiento;
+    const [anoActual, mesActual, diaActual] = fechaActual;
+
+    let edadAnos = anoActual - anoNacimiento;
+    let edadMeses = mesActual - mesNacimiento;
+    let edadDias = diaActual - diaNacimiento;
+
+    if (edadDias < 0) {
+        edadDias += 30
+        edadMeses--
+    }
+
+    if (edadMeses < 0) {
+        edadMeses += 12
+        edadAnos--
+    }
+
+    return { anos: edadAnos, meses: edadMeses, dias: edadDias }
+}
+
 const edad = calculadoraEdad(fechaNacimiento, fechaActual)
 
-console.log(`Resultado final: Años: ${edad.anos}, Meses: ${edad.meses}, Días: ${edad.dias}`)
+console.log(`Resultados: Años: ${edad.anos}, Meses: ${edad.meses}, Días: ${edad.dias}`)
 alert(`Usted tiene ${edad.anos} años, ${edad.meses} meses y ${edad.dias} días.`)
